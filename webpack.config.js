5aabb5f88237cfa87ec564a768ff9bd21a7a70cd
WuIFan <denny91002@gmail.com>
2019-07-08 15:40:27 +0800

    Feature : learn react with webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    mode : 'development',
+    entry : {
+        index : "./index.jsx"
+    },
+    output : {
+        filename : "./bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react','@babel/preset-env'] } } },
+            { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } }
+        ]
+    }
+};
