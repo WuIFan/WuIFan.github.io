b712748f8d05d371b68ac7051c03b54f7683dc85
WuIFan <denny91002@gmail.com>
2021-02-26 12:25:21 +0800

    Feature: projects

--- src/component/profile_data.js
+++ src/component/profile_data.js
@@ -12,18 +12,18 @@ export const profile_data = {
       "skills": [
         { 
           "name": "Frontend",
-          "content": ["javascript","typescript","reatjs"]
+          "content": ["Javascript","Typescript","Reatjs"]
         },
         {
-           "name": "Backend",
-          "content": ["python","jquery","java","mysql","firebase"]
+          "name": "Backend",
+          "content": ["Python","Jquery","Java","MySQL","Firebase"]
         },
         {
-           "name": "Testing",
+          "name": "Testing",
           "content":  ["Selenium IDE","SideeX"]
         },
         {
-           "name": "Others",
+          "name": "Others",
           "content":  ["git","trello","hackmd"]
         }
       ],
@@ -42,15 +42,24 @@ export const profile_data = {
       "projects": [
         {
           "name": "SideeX",
-          "tool": "Typescript"
+          "tool": "Typescript",
+          "picture": "assets/img/portfolio/02-thumbnail.jpg",
+          "description": "Selenium IDE 是目前全球最熱門的開源網頁錄製與播放自動化測試軟體，每日有超過 10萬名業界測試人員在 使用，目前 SeleniumHQ 官方正以台灣成功大學李信杰教授研究團隊所研發的 SideeX 2測試軟體為開發基礎，打造新一代 Selenium IDE，而我目前為SideeX 3代的主要Committer之一。\n我在團隊中主要負責錄製及回放等相關核心部分的撰寫，主要是找出尚未解決的問題，並試著解決，來讓我們的能夠錄製出更多的測試案例，以及能夠正確回放；還有透過與團隊的共同開發，學習如何在專案中協作、溝通，並在會議裡從團隊成員中的報告，學習各種新知。",
+          "website": "https://sideex.io/"
         },
         {
           "name": "Spotify_MergePlayList",
-          "tool": "Python"
+          "tool": "Python",
+          "picture": "assets/img/portfolio/03-thumbnail.jpg",
+          "description": "因為我平常聽 Spotify 是照著新增時間聽，然後我個兩個歌單A,B，重複率有點高所以想合併，然而 Spotify 在合併歌單上並沒有太多的功能，查到的方法只有 Ctrl + a,Ctrl + c,Ctrl + v，雖然好像會照著當下的排序複製到新的歌單，但複製第二個歌單過去的時候，不重複的歌的新增時間就變成最新。而我有查到一篇使用者詢問官方類似的問題，但官方回覆是目前沒有打算新增這項 feature ，所以就透過官方提供的 api 自己寫看看，不同於學校作業，這是一支對我來講真的有用且有很多成就感的程式。",
+          "website": "https://github.com/WuIFan/Spotify_MergePlayList"
         },
         {
           "name": "Discord_Bot",
-          "tool": "Python"
+          "tool": "Python",
+          "picture": "assets/img/portfolio/01-thumbnail.jpg",
+          "description": "因為和朋友平常最常使用的軟體就是 discord ，而自己又想練習寫 bot ，再加上這群朋友也都是資工系的學生，所以就想到一個可以爬 leetcode 個人資訊的網頁的 bot ，來抓出各種資訊，讓我們來互相比較砥礪成長。",
+          "website": ""
         }
       ]
     }
--- src/component/projects.js
+++ src/component/projects.js
@@ -3,15 +3,12 @@ import React, { Component } from 'react'
 export class Projects extends Component {
     render() {
         if(this.props.data){
-            var show_project = this.props.data.projects.map(function(projects){
+            var show_project = this.props.data.projects.map(function(projects,index){
                 return (
                     <div class="col-lg-4 col-sm-6 mb-4">
                         <div class="portfolio-item">
-                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
-                                <div class="portfolio-hover">
-                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="" />
+                            <a class="portfolio-link" data-toggle="modal" href={"#portfolioModal"+(index+1)}>
+                                <img class="img-fluid" src={projects.picture} alt=""/>
                             </a>
                             <div class="portfolio-caption">
                                 <div class="portfolio-caption-heading">{projects.name}</div>
@@ -21,6 +18,36 @@ export class Projects extends Component {
                     </div>
                     )
                 })
+            var show_modal = this.props.data.projects.map(function(projects,index){
+                return (
+                    <div class="portfolio-modal modal fade" id={"portfolioModal"+(index+1)} tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-8">
+                                            <div class="modal-body">
+                                                <h2 class="text-uppercase">{projects.name}</h2>
+                                                <p class="item-intro text-muted">{projects.tool}</p>
+                                                <img class="img-fluid d-block mx-auto" src={projects.picture} alt="" />
+                                                <p>{projects.description}</p>
+                                                <ul class="list-inline">
+                                                    <li>Website: {projects.website}</li>
+                                                </ul>
+                                                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                                    <i class="fas fa-times mr-1"></i>
+                                                    Close Project
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
         }
         return (
             <section class="page-section bg-light" id="projects">
@@ -32,8 +59,9 @@ export class Projects extends Component {
                     <div class="row">
                         {show_project}
                     </div>
-            </div>
-        </section>
+                </div>
+                {show_modal}
+            </section>
         )
     }
 }
