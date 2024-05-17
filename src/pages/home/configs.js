d15d9cb18df26b1a86aa73fa069a68f6e1762cb6
WuIFan <denny91002@gmail.com>
2024-05-18 00:20:06 +0800

    feat: toolbox section

--- src/pages/home/configs.js
+++ src/pages/home/configs.js
@@ -3,4 +3,30 @@ export default {
     text: 'Hi, I am Ivan Wu,\na Frontend Engineer,\ndedicated to work, learning, and living.',
     image: require('../../assets/IMG_2017_02.JPG'),
   },
+  toolbox: {
+    categories: ['Frontend', 'Backend', 'Testing', 'Others'],
+    scores: {
+      frontend: [
+        {name: 'ReactJS', score: 100},
+        {name: 'Javascript', score: 100},
+        {name: 'HTML/CSS', score: 85},
+        {name: 'Typescript', score: 70},
+      ],
+      backend: [
+        {name: 'Java', score: 80},
+        {name: 'NodeJS', score: 80},
+        {name: 'MySQL', score: 75},
+      ],
+      testing: [
+        {name: 'Puppeteer', score: 90},
+        {name: 'Selenium IDE', score: 60},
+        {name: 'SideeX', score: 60},
+      ],
+      others: [
+        {name: 'Three.js', score: 80},
+        {name: 'WordPress', score: 80},
+        {name: 'Docker', score: 70},
+      ]
+    },
+  },
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 import configs from './configs';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa6';
+import {FaGithub, FaLinkedin, FaEnvelope} from 'react-icons/fa6';
+import classNames from 'classnames';
+import _map from 'lodash/map';
 
 export default function Home() {
   console.log('#', configs.introduce);
+  const [isBackCard, setIsBackCard] = useState(false);
+  const [activeCategory, setActiveCategory] = useState(
+    configs.toolbox.categories[0]
+  );
+
+  const handleCategoriesTabClick = (category) => {
+    setIsBackCard(!isBackCard);
+    setActiveCategory(category);
+  };
+
   return (
     <div>
       <section>
@@ -20,8 +32,50 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section>
-        <div></div>
+      <section className="iw-home-section-toolbox">
+        <h2 style={{fontSize: '42px', marginBottom: '50px'}}>My Toolbox</h2>
+        <div className="iw-home-toolbox--container">
+          <div
+            className={classNames('iw-home-toolbox-card', {
+              back: isBackCard,
+            })}>
+            <div
+              className={classNames('iw-home-toolbox-card--content', {
+                back: isBackCard,
+              })}>
+              <div className="iw-home-toolbox--tabs">
+                {_map(configs.toolbox.categories, (category) => {
+                  return (
+                    <h3
+                      className={classNames('iw-home-toolbox--tab', {
+                        active: activeCategory === category,
+                      })}
+                      onClick={() => handleCategoriesTabClick(category)}>
+                      {category}
+                    </h3>
+                  );
+                })}
+              </div>
+              <div className="iw-home-toolbox-card--bar-container">
+                {_map(
+                  configs.toolbox.scores[activeCategory.toLowerCase()],
+                  (item) => {
+                    return (
+                      <div className="iw-home-toolbox-card--bar">
+                        <div
+                          className="iw-home-toolbox-card--bar-progress"
+                          style={{width: `${item.score}%`}}>
+                          <div>{item.name}</div>
+                          <div>{item.score}%</div>
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
