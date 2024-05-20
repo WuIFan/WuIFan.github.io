e95252f020abf447cc501d0e04a565aec98b1a33
WuIFan <denny91002@gmail.com>
2024-05-20 21:46:13 +0800

    feat: developing

---
+++ src/components/developing/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function Developing() {
+  return (
+    <div className="iw-developing--container">
+      <div className="iw-developing--content">Developing</div>
+    </div>
+  );
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,13 +4,15 @@ import configs from './configs';
 import IntroduceSection from './introducesection';
 import ToolboxSection from './toolboxsection';
 import ExperienceSection from './experiencesection';
+import Developing from '../../components/developing';
 
 export default function Home() {
   return (
     <>
       <IntroduceSection configs={configs} />
       <ToolboxSection configs={configs} />
-      <ExperienceSection configs={configs} />
+      {/* <ExperienceSection configs={configs} /> */}
+      <Developing />
     </>
   );
 }
